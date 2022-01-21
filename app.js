22c76042a4dad416ea6afb26a3ca4b083b15e4a9
Joao Victor Correa Saldanha <joaosaldanha388@gmail.com>
2022-01-21 10:29:52 -0300

    fix app

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
+
 const formAddTodo = document.querySelector('.form-add-todo')
-const todosContainer = document.querySelector('.todos-container')
+const ulList = document.querySelector('.todos-container')
+const formSearchTodo = document.querySelector('.form-search')
 
-formAddTodo.addEventListener('submit', evento => {
-    evento.preventDefault()
+// adiciona novo elemento na lista 
+formAddTodo.addEventListener('submit', evento =>
+{
 
-    const inputValue = evento.target.add.value.trim() // metodo trim remove espaços em branco no inicio/fim
-    if(inputValue.length){
-          todosContainer.innerHTML += `  
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${inputValue}</span>
-    <i class="far fa-trash-alt delete"></i>
-  </li>`
+  evento.preventDefault()
+  const inputValue = evento.target.add.value.trim() 
 
-  evento.target.reset() //limpa o input
+    if(inputValue.length)
+    {
+      ulList.innerHTML += `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li> `
+      evento.target.reset()
     }
-  
+})
+
+ulList.addEventListener('click', evento=>{
+
+  const clickedElement = evento.target
+  if(Array.from(clickedElement.classList).includes('delete')){
+    clickedElement.parentElement.remove()
+  }
+})
+
+
+formSearchTodo.addEventListener('input', evento => {
+  const inputValue = evento.target.value.trim()
+  Array.from(ulList.children)
+  .filter(todo => !todo.textContent.includes(inputValue))
+  .forEach(todo => {
+
+    todo.classList.remove('d-flex')
+    todo.classList.add('hidden')
+  })
+  Array.from(ulList.children)
+  .filter(todo => todo.textContent.includes(inputValue))
+  .forEach(todo => {
+
+    todo.classList.remove('hidden')
+    todo.classList.add('d-flex')
+  })
 })
